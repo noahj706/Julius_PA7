ff88b8fe207f6f93ed5d1320b49db446063d6d49
noahj706 <noahj706@gmail.com>
2026-04-22 14:07:36 -0700

    added main into the project

---
+++ Julius_PA7/src/bin/main.cpp
@@ -0,0 +1,19 @@
+/*
+Programmer(s): Noah Julius
+Data: April 2026
+Project Title: Attendance Tracker w/ Class Templates
+Descruption: CPT_S 122 assignment, gives me practice with class templates.
+WHAT IT DOES is uses class templates to track the attendance of student along with some other meny options
+for user friendliness or smth.
+*/
+
+#include "menu.hpp"
+
+int main(void)
+{
+	Menu mainMenu;
+
+	mainMenu.run();//start wrapper
+
+	return 0;
+}
